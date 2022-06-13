c3e62ed8a591c734e1c202f49997d3e0e6a432bb
Darryl Koh Ka-Wei <s10221893@connect.np.edu.sg>
2022-06-13 22:56:35 +0800

    able to save farmer's name (in game name) using shared preferences

--- app/src/main/java/sg/edu/np/mad/madassignment1/AccountSettingsFragment.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/AccountSettingsFragment.java
@@ -1,16 +1,27 @@
 package sg.edu.np.mad.madassignment1;
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.text.BreakIterator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,11 +30,10 @@ import android.widget.Toast;
  */
 public class AccountSettingsFragment extends Fragment {
 
-    public static final String PREFS_NAME = "MyLoginPrefs";
-    private String TAG = "Account Settings Page";
-    public String GLOBAL_PREF = "MyPrefs";
-    public String MY_USERNAME = "MyUsername";
-
+    public String getString;
+    public static final String SHARED_PREF = "shared";
+    public static final String TEXT = "text";
+    public String text;
 
 //    public static final String PREFERENCES = "preferences";
 //    public static final String CUSTOM_THEME = "customTheme";
@@ -78,6 +88,7 @@ public class AccountSettingsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
@@ -86,14 +97,39 @@ public class AccountSettingsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account_settings, container, false);
         Button myButtonSave = (Button) view.findViewById(R.id.SaveBtn);
+        TextView myUserName = (TextView) view.findViewById(R.id.UserName);
+        TextView editText = (TextView) view.findViewById(R.id.EditAccountUsername);
+
+
         myButtonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                getString = editText.getText().toString();
+                myUserName.setText(getString);
+
+                // shared preferences
+                Log.v("front", "test");
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(TEXT, myUserName.getText().toString());
+                editor.apply();
+
+                // edit
+//                String username = sharedPreferences.getString("username", "default");
+//                TextView editUser = (TextView) getActivity().findViewById(R.id.EditAccountUsername);
+//                editUser.setText(username);
+
                 // Once saved, go back to Settings page
                 Navigation.findNavController(view).navigate(R.id.action_nav_AccountSettings_to_nav_Settings); //call the action
+                // Display "Saved"
                 Toast.makeText(getActivity(), "Saved", Toast.LENGTH_SHORT).show();
             }
         });
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "");
+        myUserName.setText(text);
         return view;
     }
+
 }
