a0dd4f37c9f773962823ac9b6cd76ed68e7ce805
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-16 07:52:34 +0800

    deleted more unused files, added comments to DrawerBaseActivity

--- app/src/main/java/sg/edu/np/mad/madassignment1/DrawerBaseActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/DrawerBaseActivity.java
@@ -16,27 +16,38 @@ import android.widget.FrameLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-
+//this DrawerBaseActivity to be extended to all activities which need a hamburger icon
 public class DrawerBaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    //define navbar drawer
     DrawerLayout drawerLayout;
 
-    //set the nav bar
+    //set the nav bar and inflate it
     @Override
     public void setContentView(View view) {
+        //inflate
         drawerLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_drawer_base, null);
+        //define the fragment container(framelayout) which activities will be placed into
         FrameLayout container = drawerLayout.findViewById(R.id.activityContainer);
+        //add our view, the activity into the container
         container.addView(view);
+        //set contents to the nav bar drawer
         super.setContentView(drawerLayout);
 
+        //define tool bar defined in content_layout.xml
         Toolbar toolbar = drawerLayout.findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //gets the navigation view from somewhere idk help find
+        //create listener for nav_ACTIVITYNAME for switching nav items
         NavigationView navigationView = drawerLayout.findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //drawer closing and opening state
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.menu_drawer_open, R.string.menu_drawer_close);
+        //add listener which toggle between open and close
         drawerLayout.addDrawerListener(toggle);
+        //sync to all activities
         toggle.syncState();
     }
 
@@ -48,9 +59,14 @@ public class DrawerBaseActivity extends AppCompatActivity implements NavigationV
 
         //calender button
         switch (item.getItemId()){
+            //link nav bar button to this condition (R.id.NAVBUTTONNAME)
+            //nav button name in res>menu>main_drawer_menu.xml>code
             case R.id.nav_calendar:
+                //starts activity by intent
                 startActivity(new Intent(this,CalenderActivity.class));
+                //set animation
                 overridePendingTransition(0,0);
+                //break out of this switch case and return false, exiting this
                 break;
         }
 
@@ -69,6 +85,30 @@ public class DrawerBaseActivity extends AppCompatActivity implements NavigationV
                 overridePendingTransition(0,0);
                 break;
         }
+
+        //Timer button
+        switch (item.getItemId()){
+            case R.id.nav_Timer:
+                startActivity(new Intent(this,TaskActivity.class));
+                overridePendingTransition(0,0);
+                break;
+        }
+
+        //Settings button
+        switch (item.getItemId()){
+            case R.id.nav_Settings:
+                startActivity(new Intent(this,SettingsActivity.class));
+                overridePendingTransition(0,0);
+                break;
+        }
+
+//        //Log Out button
+//        switch (item.getItemId()){
+//            case R.id.nav_Logout:
+//                startActivity(new Intent(this,LogOutActivity.class));
+//                overridePendingTransition(0,0);
+//                break;
+//        }
 
         return false;
     }
