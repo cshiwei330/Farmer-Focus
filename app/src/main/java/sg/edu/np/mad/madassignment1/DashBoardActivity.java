58bb24967b6aa4141bc82d038173466f101a27b1
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-16 02:04:33 +0800

    Recreated  hamburger navigation using ACTIVITES
    indians are actually straight up built different
    https://www.youtube.com/watch?v=pRieCkF1Yts

---
+++ app/src/main/java/sg/edu/np/mad/madassignment1/DashBoardActivity.java
@@ -0,0 +1,19 @@
+package sg.edu.np.mad.madassignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sg.edu.np.mad.madassignment1.databinding.ActivityDashBoardBinding;
+
+public class DashBoardActivity extends DrawerBaseActivity {
+
+    ActivityDashBoardBinding activityDashBoardBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityDashBoardBinding = ActivityDashBoardBinding.inflate(getLayoutInflater());
+        setContentView(activityDashBoardBinding.getRoot());
+    }
+}
