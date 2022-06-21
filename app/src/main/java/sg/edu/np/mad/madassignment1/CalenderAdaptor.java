4ee0971dabacde668f08d4d6f18afa33ce323e4f
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-21 13:52:32 +0800

    fixed a bug where month was showing wrong

--- app/src/main/java/sg/edu/np/mad/madassignment1/CalenderAdaptor.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/CalenderAdaptor.java
@@ -127,7 +127,7 @@ public class CalenderAdaptor extends RecyclerView.Adapter<CalenderViewHolder> im
         //convert int data to string
         String stringDate = String.format("%d/%d/%d",t.getTaskDayOfMonth(),t.getTaskMonth(),t.getTaskYear());
         //date format
-        SimpleDateFormat fmt = new SimpleDateFormat("dd/mm/yyyy");
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
         //give dumby value to taskDate for forced initialization
         Date taskDate = null;
 
