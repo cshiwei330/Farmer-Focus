796aa76f89df66e9ef2fd045d55f09ae327ab539
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-21 17:20:47 +0800

    remade calenderView using recyclerview

---
+++ app/src/main/java/sg/edu/np/mad/madassignment1/CalenderViewAdaptor.java
@@ -0,0 +1,49 @@
+package sg.edu.np.mad.madassignment1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CalenderViewAdaptor extends RecyclerView.Adapter<CalenderViewViewHolder> {
+    private final ArrayList<String> daysOfMonth;
+    private final OnItemListener onItemListener;
+
+    public CalenderViewAdaptor(ArrayList<String> daysOfMonth, OnItemListener onItemListener)
+    {
+        this.daysOfMonth = daysOfMonth;
+        this.onItemListener = onItemListener;
+    }
+
+    @NonNull
+    @Override
+    public CalenderViewViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.calender_cell, parent, false);
+        ViewGroup.LayoutParams layoutParams = view.getLayoutParams();
+        layoutParams.height = (int) (parent.getHeight() * 0.166666666);
+        return new CalenderViewViewHolder(view, onItemListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CalenderViewViewHolder holder, int position)
+    {
+        holder.dayOfMonth.setText(daysOfMonth.get(position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return daysOfMonth.size();
+    }
+
+    public interface  OnItemListener
+    {
+        void onItemClick(int position, String dayText);
+    }
+}
