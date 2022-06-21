55e7c8a83cbbd4c8aaca93e5af8ccf995c90a699
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-21 21:48:55 +0800

    fixed task deletion by reducing the specificity, might not be the best but it works

--- app/src/main/java/sg/edu/np/mad/madassignment1/DBHandler.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/DBHandler.java
@@ -195,12 +195,16 @@ public class DBHandler extends SQLiteOpenHelper {
 
     public void deleteTask(Task deleteTask){ // String taskName, String taskDesc
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete("Tasks","TaskId=? and TaskStatus=? and TaskName=? and TaskDesc=?" +
-                        " and TaskHour=? and TaskMinute=? and TaskYear=? and TaskMonth=? and TaskDayOfMonth=?",
-                new String[]{String.valueOf(deleteTask.getId()), String.valueOf(deleteTask.getStatus()),
-                        deleteTask.getTaskName(), deleteTask.getTaskDesc(), String.valueOf(deleteTask.getTaskHour()),
-                        String.valueOf(deleteTask.getTaskMinute()), String.valueOf(deleteTask.getTaskYear()),
-                        String.valueOf(deleteTask.getTaskMonth()), String.valueOf(deleteTask.getTaskDayOfMonth())});
+
+        // THERE IS SOMETHING WRONG HERE AND I CANT TELL WHY
+//        db.delete("Tasks","TaskId=? and TaskStatus=? and TaskName=? and TaskDesc=? and TaskHour=? and TaskMinute=? and TaskYear=? and TaskMonth=? and TaskDayOfMonth=?",
+//                new String[]{String.valueOf(deleteTask.getId()), String.valueOf(deleteTask.getStatus()),
+//                        deleteTask.getTaskName(), deleteTask.getTaskDesc(), String.valueOf(deleteTask.getTaskHour()),
+//                        String.valueOf(deleteTask.getTaskMinute()), String.valueOf(deleteTask.getTaskYear()),
+//                        String.valueOf(deleteTask.getTaskMonth()), String.valueOf(deleteTask.getTaskDayOfMonth())});
+
+        //shortened .delete command to delete task by id
+        db.delete("Tasks","TaskId=?", new String[]{String.valueOf(deleteTask.getId())});
 
         Log.v(TAG, "Task Deleted");
         // db.delete("Tasks","TaskName=? and TaskDesc=?",new String[]{taskName,taskDesc});
