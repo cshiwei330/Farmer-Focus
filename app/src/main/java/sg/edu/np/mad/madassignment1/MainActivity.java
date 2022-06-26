0369eae9b2093896e148166ac48fa7636b294864
iyaki01 <s10219179@connect.np.edu.sg>
2022-06-26 16:39:18 +0800

    fixed bug with deleted database causing crash when restarting

--- app/src/main/java/sg/edu/np/mad/madassignment1/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,10 +24,14 @@ public class MainActivity extends AppCompatActivity {
             SharedPreferences sharedPreferences = getSharedPreferences(GLOBAL_PREF, 0);
             // getting value
             String remember = sharedPreferences.getString("remember", "");
+            // get user name
+            String username = sharedPreferences.getString("username", "");
 //            User user = new User();
 //            // getting stored username
 //            String username = sharedPreferences.getString("username", "");
 //            user.setUsername(username);
+
+
 
             Intent intent;
             if (remember.equals("false")) {
@@ -34,8 +39,23 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(MainActivity.this, LoginPageActivity.class);
             }
             else {
-                // starting new activity, bringing user to home page if the user checked the remember me box
-                intent = new Intent(MainActivity.this, HomeActivity.class);
+                //define dbHandler
+                DBHandler dbHandler = new DBHandler(this, null, null,6);
+
+                // getting stored username
+                User user = dbHandler.findUser(username);
+
+                //if the user is found  go to homeactivity
+                if (user != null){
+                    // starting new activity, bringing user to home page if the user checked the remember me box
+                    intent = new Intent(MainActivity.this, HomeActivity.class);
+                }
+                else{
+                    Log.v("MainActivity","userID not found, database may be deleted, returning to LoginPageActivity");
+                    // starting new activity, bringing user to login page if the user did not check the remember me box
+                    intent = new Intent(MainActivity.this, LoginPageActivity.class);
+                }
+
             }
             startActivity(intent);
             finish();
