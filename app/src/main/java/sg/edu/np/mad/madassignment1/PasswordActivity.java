ac80bd292799a2cf9bf454d7d421a1a2d9a1d730
Darryl Koh Ka-Wei <s10221893@connect.np.edu.sg>
2022-06-28 22:56:21 +0800

    fixed problems with password. now if users were to enter the same password for the new password as the old password, a error message will be displayed

--- app/src/main/java/sg/edu/np/mad/madassignment1/PasswordActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/PasswordActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.jar.Pack200;
+
 public class PasswordActivity extends AppCompatActivity {
     String newPass;
     @Override
@@ -22,7 +24,7 @@ public class PasswordActivity extends AppCompatActivity {
 
         EditText myEditUsername = findViewById(R.id.EditUsername);
         EditText myOldPassword = findViewById(R.id.OldPassword);
-        EditText myPassword = findViewById(R.id.NewPassword);
+        EditText myNewPassword = findViewById(R.id.NewPassword);
         EditText myConfirmPassword = findViewById(R.id.ConfirmNewPassword);
 
         Button myButtonSave = findViewById(R.id.SaveBtn2);
@@ -39,32 +41,36 @@ public class PasswordActivity extends AppCompatActivity {
                         break;
                     }
                     else{
-                        if(myOldPassword.getText().toString().equals(userDBData.getPassword())){
-                            if(myPassword.getText().toString().equals(myConfirmPassword.getText().toString())){
-                                newPass = myConfirmPassword.getText().toString();
-                                userDBData.setPassword(newPass);
-                                dbHandler.updateUser(userDBData);
-                                Toast.makeText(PasswordActivity.this, "Password successfully changed!", Toast.LENGTH_SHORT).show();
+                        // ensure that the password entered is valid
+                        if(myOldPassword.getText().toString().equals(userDBData.getPassword()) && !(myOldPassword.getText().toString()).equals("")){
+                            // ensure that the new password is not equals to the original password
+                            if(!(myNewPassword.getText().toString()).equals(myOldPassword.getText().toString()) && !(myNewPassword.getText().toString()).equals("")) {
+                                // ensure that the new password must be the same as the confirm password
+                                if (myNewPassword.getText().toString().equals(myConfirmPassword.getText().toString()) && !(myConfirmPassword.getText().toString()).equals("")) {
+                                    newPass = myConfirmPassword.getText().toString();
+                                    userDBData.setPassword(newPass);
+                                    dbHandler.updateUser(userDBData);
+                                    Toast.makeText(PasswordActivity.this, "Password successfully changed!", Toast.LENGTH_SHORT).show();
 
-                                // Display "Saved" msg
-                                //Toast.makeText(PasswordActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                                    //kill TaskActivity
+                                    ((ResultReceiver) getIntent().getParcelableExtra("finisher")).send(1, new Bundle());
 
-                                //kill TaskActivity
-                                ((ResultReceiver)getIntent().getParcelableExtra("finisher")).send(1, new Bundle());
+                                    //create intent to go to Password
+                                    Intent AccountSettingsToPasswordActivity = new Intent(PasswordActivity.this, AccountSettingsActivity.class);
 
-                                //create intent to go to Password
-                                Intent AccountSettingsToPasswordActivity = new Intent(PasswordActivity.this, AccountSettingsActivity.class);
+                                    //start activity with result
+                                    startActivityForResult(AccountSettingsToPasswordActivity, 1);
 
-                                //start activity with result
-                                startActivityForResult(AccountSettingsToPasswordActivity,1);
-
-                                //kill this activity
-                                finish();
+                                    //kill this activity
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(PasswordActivity.this, "Passwords do not match. Please try again.", Toast.LENGTH_SHORT).show();
+                                }
                             }
                             else{
-                                Toast.makeText(PasswordActivity.this, "Passwords do not match. Please try again.", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(PasswordActivity.this, "Password already exists. Please enter a different one.", Toast.LENGTH_SHORT).show();
                             }
-
                         }
                         else{
                             Toast.makeText(PasswordActivity.this, "Invalid Password. Please try again.", Toast.LENGTH_SHORT).show();
