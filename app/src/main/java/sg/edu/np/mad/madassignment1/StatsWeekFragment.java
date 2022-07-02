e598975f40f22a28aae367bdb8bd3284b1d90b83
Yu Xin Aislinn Tan <s10222271@connect.np.edu.sg>
2022-07-02 23:51:23 +0800

    added stats of the week fragment, no code implemented yet

---
+++ app/src/main/java/sg/edu/np/mad/madassignment1/StatsWeekFragment.java
@@ -0,0 +1,34 @@
+package sg.edu.np.mad.madassignment1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StatsWeekFragment extends Fragment {
+
+
+    public StatsWeekFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_stats_week, container, false);
+
+
+
+
+        return view;
+    }
+}
