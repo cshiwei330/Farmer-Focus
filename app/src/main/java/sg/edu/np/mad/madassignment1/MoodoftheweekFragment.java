7713afa752f0f81605efe77ee0d568f84707f9a3
Yu Xin Aislinn Tan <s10222271@connect.np.edu.sg>
2022-07-08 16:45:47 +0800

    fixed error for mood of the week fragment

--- app/src/main/java/sg/edu/np/mad/madassignment1/MoodoftheweekFragment.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/MoodoftheweekFragment.java
@@ -51,42 +51,41 @@ public class MoodoftheweekFragment extends Fragment {
 
             ArrayList<Mood> moodList = dbHandler.getMoodData();
 
-            Date moodDate = sdf.parse(moodList.get(0).getDate());
-            ArrayList<String> moodOnlyList = new ArrayList<>();
-            for(int i=0; i <= moodList.size(); i++){
-                moodOnlyList.add(moodList.get(1).toString());
-            }
+            if(moodList.size() > 0) {
+                Date moodDate = sdf.parse(moodList.get(0).getDate());
 
-            if(moodDate.compareTo(today)==0 || (moodDate.after(pastWeek.getTime()) && moodDate.before(today))){
-                int count = 0, maxCount = 0;
-                String motw = "";
-                for(int i = 0; i < moodOnlyList.size(); i++){
-                    count = 1;
-                    for(int j = i+1; j < moodOnlyList.size(); j++){
-                        if(moodOnlyList.get(i).equals(moodOnlyList.get(j))){
-                            count++;
+                ArrayList<String> moodOnlyList = new ArrayList<>();
+                for (int i = 0; i <= moodList.size(); i++) {
+                        moodOnlyList.add(moodList.get(0).getMood());
+                }
+
+                if (moodDate.compareTo(today) == 0 || (moodDate.after(pastWeek.getTime()) && moodDate.before(today))) {
+                    int count = 0, maxCount = 0;
+                    String motw = "";
+                    for (int i = 0; i < moodOnlyList.size(); i++) {
+                        count = 1;
+                        for (int j = i + 1; j < moodOnlyList.size(); j++) {
+                            if (moodOnlyList.get(i).equals(moodOnlyList.get(j))) {
+                                count++;
+                            }
+                        }
+                        if (count > maxCount) {
+                            maxCount = count;
+                            motw = moodOnlyList.get(i);
+
                         }
                     }
-                    if(count > maxCount){
-                        maxCount = count;
-                        motw = moodOnlyList.get(i);
-
+                    if (motw.equals("happy")) {
+                        moodStats.setText("In the past week, you have been mostly happy. Keep the positivity coming!");
+                    } else if (motw.equals("neutral")) {
+                        moodStats.setText("In the past week, you have been mostly neutral. Spend more time caring for your mental health! Here's an article to help :)");
+                    } else if (motw.equals("sad")) {
+                        moodStats.setText("In the past week, you have been mostly sad. Be kind to yourself. Here are some ways to help. ");
+                    } else if (motw.equals("stressed")) {
+                        moodStats.setText("In the past week, you have been mostly stressed. Take a breather and destress. Here are some ways do so.");
+                    } else {
+                        moodStats.setText(motw);
                     }
-                }
-                if (motw.equals("happy")){
-                    moodStats.setText("In the past week, you have been mostly happy. Keep the positivity coming!");
-                }
-                else if (motw.equals("neutral")){
-                    moodStats.setText("In the past week, you have been mostly neutral. Spend more time caring for your mental health! Here's an article to help :)");
-                }
-                else if (motw.equals("sad")){
-                    moodStats.setText("In the past week, you have been mostly sad. Be kind to yourself. Here are some ways to help. ");
-                }
-                else if (motw.equals("stressed")){
-                    moodStats.setText("In the past week, you have been mostly stressed. Take a breather and destress. Here are some ways do so.");
-                }
-                else{
-                    moodStats.setText(motw);
                 }
             }
             else{
