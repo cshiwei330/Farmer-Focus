3820ee0b024f063f5dffbd17f9d542658d5a0d70
iyaki01 <s10219179@connect.np.edu.sg>
2022-07-27 00:23:22 +0800

    -added recyclerview to barn popup

--- app/src/main/java/sg/edu/np/mad/madassignment1/BarnFragment.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/BarnFragment.java
@@ -1,6 +1,7 @@
 package sg.edu.np.mad.madassignment1;
 
 import android.app.AlertDialog;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -17,6 +19,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class BarnFragment extends Fragment {
 
@@ -27,6 +31,10 @@ public class BarnFragment extends Fragment {
     private AlertDialog dialog;
     private TextView barnTaskPopUpTitle;
     private RecyclerView barnTaskPopUpRecyclerView;
+    private ArrayList<Task> taskList = new ArrayList<>();
+
+    public String GLOBAL_PREF = "MyPrefs";
+
 
     public static BarnFragment newInstance(int page, String title) {
         BarnFragment barnFragment = new BarnFragment();
@@ -51,6 +59,18 @@ public class BarnFragment extends Fragment {
         TextView tvLabel = (TextView) view.findViewById(R.id.BarnFragmentTextView);
         tvLabel.setText(page + " -- " + title + "THIS IS BARN");
 
+        //define dbHandler
+        DBHandler dbHandler = new DBHandler(this.getContext(), null, null,6);
+
+        // shared preferences to get username
+        SharedPreferences sharedPreferences = requireContext().getSharedPreferences(GLOBAL_PREF, 0);
+        String username = sharedPreferences.getString("username", "");
+        User user = dbHandler.findUser(username);
+
+        //fill taskList with db data
+
+        taskList = dbHandler.getTaskData(user.getUserID());
+
         ImageView barnImage = (ImageView) view.findViewById(R.id.BarnImageView);
         barnImage.setClickable(true);
         barnImage.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +92,14 @@ public class BarnFragment extends Fragment {
 
         barnTaskPopUpTitle = (TextView) BarnTaskPopupView.findViewById(R.id.barnTaskPopUpTitle);
         barnTaskPopUpRecyclerView = (RecyclerView) BarnTaskPopupView.findViewById(R.id.barnTaskPopUpRecyclerView);
+
+        // initialize recyclerview for TASKS
+        //set adaptor to TaskRecyclerViewAdaptor, given taskList
+        BarnTaskAdapter barnTaskAdapter = new BarnTaskAdapter(taskList);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this.getContext());
+        barnTaskPopUpRecyclerView.setLayoutManager(mLayoutManager);
+        barnTaskPopUpRecyclerView.setAdapter(barnTaskAdapter);
+
 
         dialogBuilder.setView(BarnTaskPopupView);
         dialog = dialogBuilder.create();
---
+++ app/src/main/java/sg/edu/np/mad/madassignment1/BarnTaskViewHolder.java
@@ -0,0 +1,23 @@
+package sg.edu.np.mad.madassignment1;
+
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BarnTaskViewHolder extends RecyclerView.ViewHolder{
+
+    TextView taskName;
+    TextView taskDesc;
+    TextView taskDate;
+    TextView taskTime;
+
+    public BarnTaskViewHolder(View itemView){
+        super(itemView);
+        taskName = itemView.findViewById(R.id.task_name);
+        taskDesc = itemView.findViewById(R.id.task_desc);
+        //taskDate = itemView.findViewById(R.id.taskDate);
+        taskTime = itemView.findViewById(R.id.task_time);
+    }
+}
