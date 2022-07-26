8bc1bce5edb691a9a0ee75f5d1398f7bc0263681
cshiwei330 <s10221849@connect.np.edu.sg>
2022-07-26 21:28:57 +0800

    Added method in DBHandler for appWidget

--- app/src/main/java/sg/edu/np/mad/madassignment1/WidgetProvider.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/WidgetProvider.java
@@ -5,8 +5,11 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
+import android.util.Log;
 import android.widget.RemoteViews;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -17,12 +20,14 @@ import java.util.Date;
 
 public class WidgetProvider extends AppWidgetProvider {
 
+    private String TAG = "WidgetProvider";
+
     public String GLOBAL_PREF = "MyPrefs";
     private Context context;
     private DBHandler dbHandler;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId, String GLOBAL_PREF, DBHandler dbHandler) {
+                                int appWidgetId, String GLOBAL_PREF, DBHandler dbHandler) throws ParseException {
 
         //Task Number
         ArrayList<Task> todayTaskList = new ArrayList<>();
@@ -34,9 +39,7 @@ public class WidgetProvider extends AppWidgetProvider {
         User user = dbHandler.findUser(username);
 
         // Fill taskList with current db data
-        ArrayList<Task> taskList = new ArrayList<>();
-        taskList = dbHandler.getTaskData((user.getUserID()));
-        todayTaskList = findTodayTasks(taskList);
+        todayTaskList = dbHandler.getTodayTaskData(user.getUserID());
 
         // To see how many tasks
         int taskNo = todayTaskList.size();
@@ -44,7 +47,10 @@ public class WidgetProvider extends AppWidgetProvider {
         mainView.setTextViewText(R.id.widgetTaskNo, ("   " + String.valueOf(taskNo)));
 
         // For List View
+
         Intent serviceIntent = new Intent(context, WidgetService.class);
+        serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
         mainView.setRemoteAdapter(R.id.widgetListView, serviceIntent);
         mainView.setEmptyView(R.id.widgetListView, R.id.empty);
 
@@ -55,6 +61,8 @@ public class WidgetProvider extends AppWidgetProvider {
         //mainView.setOnClickPendingIntent(R.id.widgetAddTaskButton, pendingButtonIntent);
 
         // Instruct the widget manager to update the widget
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widgetListView);
+
         appWidgetManager.updateAppWidget(appWidgetId, mainView);
 
     }
@@ -63,8 +71,19 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId, GLOBAL_PREF, dbHandler);
+
+            Log.v(TAG, "Update");
+
+            try {
+                updateAppWidget(context, appWidgetManager, appWidgetId, GLOBAL_PREF, dbHandler);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            // will trigger onDataSetChanged() in WidgetService
             appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.widgetListView);
+
+            Log.v(TAG, "Update completed");
         }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
@@ -80,8 +99,7 @@ public class WidgetProvider extends AppWidgetProvider {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-
-    public static ArrayList<Task> findTodayTasks(ArrayList<Task> taskList){
+    /*public static ArrayList<Task> findTodayTasks(ArrayList<Task> taskList){
 
         ArrayList<Task> recentTaskList = new ArrayList<>();
 
@@ -120,6 +138,6 @@ public class WidgetProvider extends AppWidgetProvider {
             e.printStackTrace();
             return false;
         }
-    }
+    }*/
 
 }
