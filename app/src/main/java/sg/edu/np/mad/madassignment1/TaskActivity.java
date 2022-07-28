8252f5c8c2d08e2fbb0a49e89a2b8754ec91af51
Radchanon Don Sukkram <S10223354@connect.np.edu.sg>
2022-07-28 22:29:42 +0800

    Replaced notification icon, Fixed position of edit task button in activity_view_task.xml, Fixed filter crash issue in Task.java

--- app/src/main/java/sg/edu/np/mad/madassignment1/TaskActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment1/TaskActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
