be3e8c2f24ce8ab23c3247d742466644999fb40a
iyaki01 <s10219179@connect.np.edu.sg>
2022-07-28 23:26:50 +0800

    -redid farm data -half done silo fragment -created adapter and holder for silo popup

---
+++ app/src/main/java/sg/edu/np/mad/madassignment1/SiloTaskAdapter.java
@@ -0,0 +1,38 @@
+package sg.edu.np.mad.madassignment1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SiloTaskAdapter extends RecyclerView.Adapter<SiloTaskViewHolder> {
+
+    private ArrayList<ArrayList<Task>> data;
+
+    public SiloTaskAdapter(ArrayList<ArrayList<Task>> input){ data = input;}
+
+    @NonNull
+    @Override
+    public SiloTaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.myfarm_silo_task_view_holder,parent,false);
+        return new SiloTaskViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SiloTaskViewHolder holder, int position) {
+        //define task array list as recurringUnique
+        ArrayList<Task> recurringUnique = data.get(position);
+
+        holder.taskName.setText(recurringUnique.get(0).getTaskName());
+        holder.taskDesc.setText(recurringUnique.get(0).getTaskDesc());
+
+        holder.timesCompleted.setText(recurringUnique.size());
+    }
+
+    @Override
+    public int getItemCount() { return data.size(); }
+}
